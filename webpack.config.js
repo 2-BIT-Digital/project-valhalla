c4bfc9996ae741995d9e63d5aaeef0a9b367ae2f
smangio <smangio4@gmail.com>
2017-05-09 18:32:56 +1000

    Add project beginnings
    Adds a lot of shit like craft CMS, composer, webpack, babel etc etc

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  context: path.resolve(__dirname, './craft/templates'),
+  entry: {
+    main: './js/main.js',
+  },
+  output: {
+    path: path.resolve(__dirname, './public/dist'),
+    filename: '[name].bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: [/node_modules/],
+        use: [{
+          loader: 'babel-loader',
+          options: { presets: ['es2015'] }
+        }],
+      },
+      {
+        test: /\.sass$/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          loader: "css-loader!sass-loader",
+        }),
+      },
+    ],
+  },
+  plugins: [
+    new ExtractTextPlugin({
+      filename: '[name].bundle.css',
+      allChunks: true,
+    }),
+  ],
+};
