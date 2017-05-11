a46384c5c76fe90c976249d9ea819ef278f78176
smangio <smangio4@gmail.com>
2017-05-11 15:29:22 +1000

    Remove DB credentials from PHP

--- craft/config/db.php
+++ craft/config/db.php
@@ -1,25 +1,19 @@
 <?php
-
 /**
  * Database Configuration
  *
  * All of your system's database configuration settings go in here.
  * You can see a list of the default settings in craft/app/etc/config/defaults/db.php
  */
-
 return array(
 	// The database server name or IP address. Usually this is 'localhost' or '127.0.0.1'.
-	'server' => getenv('DB_HOST') ?: 'aa1wfcw41q5qii2.cw1xhmvwsxme.us-west-2.rds.amazonaws.com:3306',
-
+	'server' => getenv('DB_HOST') ?: 'localhost',
 	// The name of the database to select.
-	'database' => getenv('DB_NAME') ?: 'valhalla',
-
+	'database' => getenv('DB_NAME') ?: '',
 	// The database username to connect with.
-	'user' => getenv('DB_USER') ?: 'valhalla',
-
+	'user' => getenv('DB_USER') ?: 'root',
 	// The database password to connect with.
-	'password' => getenv('DB_PASS') ?: 'valhalla',
-	
+	'password' => getenv('DB_PASS') ?: '',
 	// The prefix to use when naming tables. This can be no more than 5 characters.
 	'tablePrefix' => 'craft',
 );
