732ed15459fe158133228ad96d2232a2c3e51f6a
smangio <smangio4@gmail.com>
2017-05-11 13:23:44 +1000

    Update db.php

--- craft/config/db.php
+++ craft/config/db.php
@@ -9,16 +9,16 @@
 
 return array(
 	// The database server name or IP address. Usually this is 'localhost' or '127.0.0.1'.
-	'server' => getenv('DB_HOST') ?: 'localhost',
+	'server' => getenv('DB_HOST') ?: 'aa1wfcw41q5qii2.cw1xhmvwsxme.us-west-2.rds.amazonaws.com:3306',
 
 	// The name of the database to select.
 	'database' => getenv('DB_NAME') ?: 'valhalla',
 
 	// The database username to connect with.
-	'user' => getenv('DB_USER') ?: 'homestead',
+	'user' => getenv('DB_USER') ?: 'valhalla',
 
 	// The database password to connect with.
-	'password' => getenv('DB_PASS') ?: 'secret',
+	'password' => getenv('DB_PASS') ?: 'valhalla',
 	
 	// The prefix to use when naming tables. This can be no more than 5 characters.
 	'tablePrefix' => 'craft',
