636e97971c7751ef74ad72f297a4f8c9d89c3381
smangio <smangio4@gmail.com>
2017-05-15 15:07:44 +1000

    More index.php rehauls
    It will end soon I promise

--- public/index.php
+++ public/index.php
@@ -1,35 +1,23 @@
 <?php
+// Load the local Craft environment
+if (file_exists('../.env.php'))
+    require_once '../.env.php';
 
-require_once('../vendor/autoload.php');
-
-$craftEnvironment = getenv('CRAFT_ENVIRONMENT') ?: 'staging';
-
-if ($craftEnvironment === 'development') {
-    try {
-        $dotenv = new Dotenv\Dotenv(dirname(__DIR__));
-        $dotenv->load();
-        $dotenv->required(['DB_HOST', 'DB_NAME', 'DB_USER', 'DB_PASS', 'SITE_URL']);
-    } catch (Exception $e) {
-        exit('Could not find a .env file.');
-    }
-}
-
-define('CRAFT_ENVIRONMENT', $craftEnvironment);
+// Default environment
+if (!defined('CRAFT_ENVIRONMENT'))
+    define('CRAFT_ENVIRONMENT', getenv('CRAFTENV_CRAFT_ENVIRONMENT'));
 
 // Path to your craft/ folder
 $craftPath = '../craft';
 
 // Do not edit below this line
 $path = rtrim($craftPath, '/').'/app/index.php';
-
 if (!is_file($path))
 {
     if (function_exists('http_response_code'))
     {
         http_response_code(503);
     }
-
     exit('Could not find your craft/ folder. Please ensure that <strong><code>$craftPath</code></strong> is set correctly in '.__FILE__);
 }
-
 require_once $path;
