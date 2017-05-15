835bd37a25f8cb7253bba947dfa9807225dfcb7a
smangio <smangio4@gmail.com>
2017-05-15 11:35:46 +1000

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,5 +1,19 @@
 <?php
 
+require_once('../vendor/autoload.php');
+
+$craftEnvironment = getenv('CRAFT_ENVIRONMENT') ?: 'development';
+if ($craftEnvironment === 'development') {
+    try {
+        $dotenv = new Dotenv\Dotenv(dirname(__DIR__));
+        $dotenv->load();
+        $dotenv->required(['DB_HOST', 'DB_NAME', 'DB_USER', 'DB_PASS', 'SITE_URL']);
+    } catch (Exception $e) {
+        exit('Could not find a .env file.');
+    }
+}
+define('CRAFT_ENVIRONMENT', $craftEnvironment);
+
 // Path to your craft/ folder
 $craftPath = '../craft';
 
@@ -8,12 +22,12 @@ $path = rtrim($craftPath, '/').'/app/index.php';
 
 if (!is_file($path))
 {
-	if (function_exists('http_response_code'))
-	{
-		http_response_code(503);
-	}
+    if (function_exists('http_response_code'))
+    {
+        http_response_code(503);
+    }
 
-	exit('Could not find your craft/ folder. Please ensure that <strong><code>$craftPath</code></strong> is set correctly in '.__FILE__);
+    exit('Could not find your craft/ folder. Please ensure that <strong><code>$craftPath</code></strong> is set correctly in '.__FILE__);
 }
 
 require_once $path;
